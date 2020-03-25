21309c2ada82795f943b135c4d65ec0ab76e8873
João Paulo Oliveira <jaopol@gmail.com>
2020-03-24 21:50:56 -0300

    Alterando start e conexão BD, para funcionar a aplicação pelo Docker

--- dist/infra/db.js
+++ dist/infra/db.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose = require("mongoose");
 class Database {
     constructor() {
-        //private DB_URL = "mongodb://link-db/db_portal"; Descomentar para Docker
-        this.DB_URL = "mongodb://localhost:27017/db_portal";
+        this.DB_URL = "mongodb://link-db/db_portal"; //Descomentar para Docker
     }
+    //private DB_URL = "mongodb://localhost:27017/db_portal"; 
     createConnection() {
         mongoose.connect(this.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
     }
